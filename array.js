fd3ca0bca107f79fb6ef3d4f170a79937e817ca0
VentRv <97329874+VentRv@users.noreply.github.com>
2022-03-14 22:48:46 +0800

    Update array.js

--- array.js
+++ array.js
@@ -1,19 +1,57 @@
-/*if...else...*/
-/*if (條件){
-    條件成立要做的事
-}
-else {
-    條件沒有成立要做的事
-}
-*/
+/*Array  陣列*/
 
-let score = 70
-if (score > 60) //如果有及格的話
-{  
-    console.log('得到100元')
+//let studentList = ["Vent" , "YuanZhang" , "ZhenLin"]  //中括號間為陣列的值
+//console.log('轉學前:' + studentList)
+//studentList.push('YouZhe')  //陣列名.push(欲增加陣列中的值)
+//console.log('轉學後:' , studentList)  //增加後成果
+
+/*length*/
+
+//console.log(studentList.length) //length可得值陣列中有多少筆資料
+
+//--------------------------------------------------------------
+
+/*Object 物件*/  //創造一個東西並賦予它詳細的資料 //宣告一個有屬性的物件
+
+//const card = {
+//    name: 'Vent的卡片',
+//    adress: 'JiaDing,ShangHai',
+ //   age : 17
 }
-else{
-    console.log('得到50元')
+//console.log(card)  //工作臺顯示完整物件
+
+/*以下以Instagram貼文舉例*/
+
+//const post1 = {
+//    image: 'https://33.img.com',
+//    desc: '浩威跟許文懷的友情照',
+/ /   date: 12.25
 }
+
+//const post2 = {
+//    image: 'https://44.img.com',
+//    desc: '浩威在烏克蘭英勇奮戰',
+//    date: 3.8,
+//    comment: ['好帥','我也要去']
+}
+
+//let wall = [
+//    post1,
+//    post2,
+]
+//console.log('第一筆:',wall[0])  //顯示陣列中的值，注意!陣列中的第一筆資料在陣列中編號為0
+//console.log('第二筆:',wall[1])
+//console.log('第五筆:',wall[4])  //陣列並沒有第四筆資料，所以顯示undefined
+
+//console.log(card.name)  //可單獨拿出物件中的屬性
+
+
+
+
+
+
+
+
+
 
 
