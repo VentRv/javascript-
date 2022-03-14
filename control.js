5e4b113a64ec74b385c91a4ed693a52349d6b469
VentRv <97329874+VentRv@users.noreply.github.com>
2022-03-14 23:05:22 +0800

    Update control.js

--- control.js
+++ control.js
@@ -7,7 +7,7 @@ else {
 }
 */
 
-let score = 70
+/*let score = 70
 if (score > 60) //如果有及格的話
 {  
     console.log('得到100元')
@@ -15,5 +15,34 @@ if (score > 60) //如果有及格的話
 else{
     console.log('得到50元')
 }
+if (score >= 100)
+{
+    console.log('你很棒')
+}
+else if (score >= 80){
+    console.log('繼續保持')
+}
+else if (score >= 60){
+    console.log('剛好及格')
+}
+else {
+    console.log('就不能學學呂承翰嗎?')
+}*/
+/* switch */
+/* seitch (key){
+
+    case value:
+
+      break;
+    case value:
+
+      break;
+    case value:
+
+      break;
+    default:
+
+      break;
+}  */
 
 
