03de983bc37217e24517d25b49b2738d716dd275
VentRv <97329874+VentRv@users.noreply.github.com>
2022-03-14 22:42:10 +0800

    Create control.js

---
+++ control.js
@@ -0,0 +1,19 @@
+/*if...else...*/
+/*if (條件){
+    條件成立要做的事
+}
+else {
+    條件沒有成立要做的事
+}
+*/
+
+let score = 70
+if (score > 60) //如果有及格的話
+{  
+    console.log('得到100元')
+}
+else{
+    console.log('得到50元')
+}
+
+
