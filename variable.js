b89e261e8b6f475ad285463ac897297eb4defad0
VentRv <97329874+VentRv@users.noreply.github.com>
2022-03-14 22:46:34 +0800

    Update variable.js

--- variable.js
+++ variable.js
@@ -2,46 +2,46 @@
 
  console.log(/*變數名稱*/)  //在工作臺執行成果
 
-let myName = "我的名字:" //string 字串 雙引號間等於string
-console.log(myName)
+//let myName = "我的名字:" //string 字串 雙引號間等於string
+//console.log(myName)
 
-let vent = "Vent"
-console.log(myName + vent)  //字串可以結合
+//let vent = "Vent"
+//console.log(myName + vent)  //字串可以結合
 
 //--------------------------------------------------
 /*number 數字*/
-let number1 = 1000  
-console.log(number1)
+//let number1 = 1000  
+//console.log(number1)
 
-let number2 = 4
-console.log(number1 + number2) //數字可加+減-乘*除/ 求餘數%
+//let number2 = 4
+//console.log(number1 + number2) //數字可加+減-乘*除/ 求餘數%
 
-let myBirthday = "我的生日:"
-let number3 = (number1 + number2)
-console.log(myBirthday + number1 +  number2)  //文字加字串皆為字串
-console.log(myBirthday + number3)  //文字可與數字結合
+//let myBirthday = "我的生日:"
+//let number3 = (number1 + number2)
+//console.log(myBirthday + number1 +  number2)  //文字加字串皆為字串
+//console.log(myBirthday + number3)  //文字可與數字結合
 
-let score = 60
-console.log(score + 20)  //變數加數字
+//let score = 60
+//console.log(score + 20)  //變數加數字
 
-console.log(100 - 60)  //數字減法
-console.log(20*30)  //數字乘法
-console.log(66/3)  //數字除法
-console.log(100%3)  //求餘數
+//console.log(100 - 60)  //數字減法
+//console.log(20*30)  //數字乘法
+//console.log(66/3)  //數字除法
+//console.log(100%3)  //求餘數
 
 //---------------------------------------------------
 
 /*true,false為布林值*/
 
-let isFake = true  //true or false
-console.log(isFake)
+//let isFake = true  //true or false
+//console.log(isFake)
 
 /*undefined,null*/
 
-let bag1 = undefined  //將變數bag1其值設為空
-let bag2 = null  //將變數bag2其值設為空
-console.log(bag1) 
-console.log(bag2)
+//let bag1 = undefined  //將變數bag1其值設為空
+//let bag2 = null  //將變數bag2其值設為空
+//console.log(bag1) 
+//console.log(bag2)
 
 //---------------------------------------------------
 
@@ -49,8 +49,8 @@ console.log(bag2)
 //皆為宣告變數，let為可變數，const為常數，後續無法修改 
 
 /*ex:*/let a1 = 135
-a1 = 246  //修改a1
-console.log(a1)
+//a1 = 246  //修改a1
+//console.log(a1)
 /*const無法改變自行測試*/
 
 //------------------------------------------------------
